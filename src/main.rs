c3b9e80050a05cac77b7b11d705fdb1d1fbd3fea
Peter Jiang <w224jian@uwaterloo.ca>
2021-12-21 21:18:57 -0500

    state management test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+
+mod mc_server;
+
 #[macro_use] extern crate rocket;
-use rocket::response::content;
+
+use std::sync::{Mutex, Arc};
+
+use rocket::request::Outcome;
+use rocket::{response::content, request::FromRequest, request::Request};
+use rocket::{State, request};
 use serde::{Serialize, Deserialize};
 use serde_json::{Result, Value};
-use rocket::tokio::time::{sleep, Duration};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+struct HitCount {
+    count: AtomicUsize
+}
+
+// #[rocket::async_trait]
+// impl<'r> FromRequest<'r> for HitCount {
+//     type Error = ();
+//     async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+//         Outcome::Success(HitCount{count: AtomicUsize::new(1)})
+//     }
+// }
 
 #[get("/versions/<rtype>")]
 async fn versions(rtype: String) -> content::Json<String> {
-    delay();
     let response: Response = serde_json::from_str(minreq::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
     .send().unwrap().as_str().unwrap()).unwrap();
     let mut r = Vec::new();
@@ -25,11 +44,18 @@ async fn server(version: String) -> content::Json<String> {
     for version_indiv in response.versions {
         if version_indiv.id == version {
            let response : Value = serde_json::from_str(minreq::get(version_indiv.url).send().unwrap().as_str().unwrap()).unwrap();
-           return content::Json(response["downloads"]["server"]["url"].to_string());
+           content::Json(response["downloads"]["server"]["url"].to_string());
         }
     }
     content::Json("error".to_string())
     
+}
+
+#[get("/count")]
+async fn test(hit_count: &State<HitCount>) -> String {
+    let current_count = hit_count.count.load(Ordering::Relaxed);
+    hit_count.count.store(current_count + 1, Ordering::Relaxed);
+    format!("Number of visits: {}", current_count)
 }
 
 fn delay() {
@@ -56,8 +82,9 @@ struct Response {
 
 #[launch]
 fn rocket() -> _ {
-
     // let response = minreq::get("http://launchermeta.mojang.com/mc/game/version_manifest.json").send().unwrap();
     // println!("{}", response.as_str().unwrap());
-    rocket::build().mount("/", routes![versions, server])
+    rocket::build().mount("/", routes![versions, server, test]).manage(HitCount { count: AtomicUsize::new(0) })
+
+
 }
