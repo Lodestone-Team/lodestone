d47fe0a563762d98faebc0522ed06b67872ec92e
Ynng <44907675+Ynng@users.noreply.github.com>
2022-01-02 21:50:03 -0500

    got cors working

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,39 @@ use futures_util::lock::Mutex;
 use std::env;
 use std::sync::Arc;
 mod handlers;
-mod util;
 mod managers;
-use managers::*;
+mod util;
 use handlers::*;
-use users::*;
 use instance_manager::InstanceManager;
+use managers::*;
 use mongodb::{options::ClientOptions, sync::Client};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
 pub struct MyManagedState {
     instance_manager: Arc<Mutex<InstanceManager>>,
     download_status: CHashMap<String, (u64, u64)>,
     mongodb_client: Client,
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
+        res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        res.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, DELETE"));
+        res.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        res.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
 
 
@@ -55,11 +77,15 @@ async fn rocket() -> _ {
             ],
         )
         .manage(MyManagedState {
-            instance_manager: Arc::new(Mutex::new(InstanceManager::new(
-                format!("{}/InstanceTest/", env::current_dir().unwrap().display()),
-                client.clone(),
-            ).unwrap())),
+            instance_manager: Arc::new(Mutex::new(
+                InstanceManager::new(
+                    format!("{}/InstanceTest/", env::current_dir().unwrap().display()),
+                    client.clone(),
+                )
+                .unwrap(),
+            )),
             download_status: CHashMap::new(),
             mongodb_client: client,
         })
+        .attach(CORS)
 }
