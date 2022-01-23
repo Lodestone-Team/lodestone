e6a3354ac55366d2ee26e8ca0005fce45643a211
Peter Jiang <w224jian@uwaterloo.ca>
2022-01-22 21:38:01 -0500

    properly handle deleting running instance

--- src/managers/instance_manager.rs
+++ src/managers/instance_manager.rs
@@ -153,22 +153,18 @@ impl InstanceManager {
     // TODO: basically drop database
     pub fn delete_instance(&mut self, uuid : String) -> Result<(), String> {
         use crate::server_instance::Status;
-        self.instance_collection.get(&uuid).ok_or("instance does not exist".to_string())?;        
-        match self.instance_collection.remove(&uuid) {
-            None => Err("instance not found".to_string()),
-            Some(instance) => {
-                if instance.get_status() == Status::Stopped {
-                    self.mongodb
+        let instance = self.instance_collection.get(&uuid).ok_or("instance does not exist".to_string())?;        
+        
+        match instance.get_status() {
+            Status::Stopped => {
+                self.mongodb
                     .database(&uuid)
                     .drop(None)
                     .unwrap();
                     fs::remove_dir_all(format!("instances/{}", instance.name)).map_err(|e| e.to_string())?;
                     return Ok(())
-                }
-                Err("instance is running".to_string())
-                // handling db
-                
-            }
+            },
+            _ => return Err("instance is running".to_string())
         }
     }
 
