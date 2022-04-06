86c44012e2a787e8a9fe1004bd5458716d7da2e8
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-04-05 20:12:10 -0400

    minor fix

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -8,7 +8,7 @@ import SystemMonitor from "../components/SystemMonitor";
 
 const devServerContext = {
   pollrate: 5000,
-  api_domain: `http://${location.hostname}:8000`,
+  api_domain: `http://${window.location.hostname}:8000`,
   api_path: "/api/v1",
 }
 
