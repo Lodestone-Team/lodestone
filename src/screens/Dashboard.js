c03fd85736a70098163d738f425ded27546869ad
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-04-16 18:09:49 -0400

    prototype for custom api domain

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,34 +1,59 @@
 import "./Dashboard.scss";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import InstanceList from "../components/InstanceList";
 import { ServerContext } from "../contexts/ServerContext";
 import SystemMonitor from "../components/SystemMonitor";
 
-const devServerContext = {
+const defaultServerContext = {
   pollrate: 5000,
-  api_domain: `http://${window.location.hostname}:8000`,
-  api_path: "/api/v1",
-}
-
-const prodServerContext = {
-  pollrate: 2500,
-  api_domain: "",
+  api_domain: "127.0.0.1:8000",
   api_path: "/api/v1",
 }
 
 export default function Dashboard() {
-  // eslint-disable-next-line no-unused-vars
-  const [serverContext, setServerContext] = useState((process.env.NODE_ENV !== "production") ? devServerContext : prodServerContext);
+  //grab serverContext from localStorage or set default
+  const [serverContext, setServerContext] = useState(
+    JSON.parse(localStorage.getItem("serverContext")) || defaultServerContext
+  );
+
+  //update serverContext in localStorage everytime it changes using useEffect hook
+  useEffect(() => {
+    localStorage.setItem("serverContext", JSON.stringify(serverContext));
+  }, [serverContext]);
+
 
   return (
-    <ServerContext.Provider value={serverContext}>
-      <div className="dashboard">
-        <h1 className="title">Dashboard</h1>
-        <InstanceList />
-        <SystemMonitor />
+    <div className="dashboard-wrapper">
+      <ServerContext.Provider value={serverContext}>
+        <div className="dashboard">
+          <h1 className="title">Dashboard</h1>
+          <InstanceList />
+          <SystemMonitor />
+        </div>
+      </ServerContext.Provider>
+      <div className="sidebar">
+        {/* crude server context setting */}
+        <div className="setting">
+          <h3>Temporary Settings</h3>
+          <div className="setting-input">
+            <label htmlFor="api_domain">Client Address</label>
+            <input
+              type="text"
+              name="api_domain"
+              value={serverContext.api_domain}
+              onChange={(e) =>
+                setServerContext({
+                  pollrate: serverContext.pollrate,
+                  api_domain: e.target.value,
+                  api_path: serverContext.api_path,
+                })
+              }
+            />
+          </div>
+        </div>
       </div>
-    </ServerContext.Provider>
+    </div>
   )
 }
