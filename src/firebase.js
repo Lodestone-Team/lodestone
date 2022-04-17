fe60a6e04141f093d5b9c220272c042257b9593b
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-04-16 21:31:24 -0400

    updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,13 @@ import {
   addDoc,
 } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyBODDeIiJCLJBVQaWhkUVJLxBGhjrHM5Gc",
+  authDomain: "lodestone-77d6a.firebaseapp.com",
+  projectId: "lodestone-77d6a",
+  storageBucket: "lodestone-77d6a.appspot.com",
+  messagingSenderId: "379853777469",
+  appId: "1:379853777469:web:b59220346f5c2da4f31854",
+  measurementId: "G-F366V1PJ2H"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
