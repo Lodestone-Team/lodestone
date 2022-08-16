03b07cbd83ce6626f59670db75adeab75eb9e25c
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-08-15 23:59:38 -0400

    Added custom font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://api.fontshare.com/css?f[]=chillax@1&f[]=satoshi@1,2&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
