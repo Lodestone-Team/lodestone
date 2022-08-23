63f18a8771bb77a9803bcbe1560858344a80a12e
Peter Jiang <2892084432j@gmail.com>
2022-08-22 21:26:07 -0400

    Redo auth

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -2,24 +2,23 @@ use std::collections::{HashMap, HashSet};
 
 use crate::{
     db::{permission::Permission, user::User},
-    handlers::util::JWT_SECRET,
     traits::{Error, ErrorInner},
+    util::rand_alphanumeric,
     AppState,
 };
 use axum::{extract::Path, Extension, Json};
 use axum_auth::{AuthBasic, AuthBearer};
 use crypto::{digest::Digest, sha3::Sha3};
 use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
-use log::debug;
 use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use systemstat::Utc;
 
-use super::util::{decode_token, is_authorized};
+use super::util::{is_authorized, try_auth};
 #[derive(Deserialize, Serialize)]
 pub struct Claim {
-    pub user: User,
+    pub uid: String,
     pub exp: usize,
 }
 
@@ -34,21 +33,20 @@ struct PermissionsUpdateSchema {
     pub permissions: HashMap<Permission, HashSet<String>>,
 }
 
-fn create_jwt(user: &User) -> Result<String, Error> {
+fn create_jwt(user: &User, jwt_secret: &str) -> Result<String, Error> {
     let exp = Utc::now()
         .checked_add_signed(chrono::Duration::days(1))
         .expect("valid timestamp")
         .timestamp();
-    debug!("exp: {}", exp);
     let claim = Claim {
-        user: user.clone(),
+        uid: user.uid.clone(),
         exp: exp as usize,
     };
     let header = Header::new(Algorithm::HS512);
     Ok(encode(
         &header,
         &claim,
-        &EncodingKey::from_secret(&*JWT_SECRET.as_bytes()),
+        &EncodingKey::from_secret(jwt_secret.as_bytes()),
     )
     .unwrap())
 }
@@ -58,7 +56,11 @@ pub async fn new_user(
     Json(config): Json<Value>,
     AuthBearer(token): AuthBearer,
 ) -> Result<Json<Value>, Error> {
-    if !is_authorized(&token, "", Permission::CanManageUser) {
+    let requester = try_auth(&token, state.users.lock().await.get_ref()).ok_or(Error {
+        inner: ErrorInner::PermissionDenied,
+        detail: "".to_string(),
+    })?;
+    if !is_authorized(&requester, "", Permission::CanManageUser) {
         return Err(Error {
             inner: ErrorInner::PermissionDenied,
             detail: "You are not authorized to create users".to_string(),
@@ -76,7 +78,7 @@ pub async fn new_user(
     let mut hasher = Sha3::sha3_256();
     hasher.input_str(format!("{}{}", salt, login_request.password).as_str());
     let hashed_psw = hasher.result_str();
-    let uuid = uuid::Uuid::new_v4().to_string();
+    let uid = uuid::Uuid::new_v4().to_string();
     let mut users = state.users.lock().await;
     if !users
         .get_ref()
@@ -90,43 +92,48 @@ pub async fn new_user(
         });
     }
     users
-        .transform(Box::new({
-            let uuid = uuid.clone();
-            move |v| {
+        .transform({
+            let uid = uid.clone();
+            Box::new(move |v| {
                 v.insert(
-                    uuid.clone(),
+                    uid.clone(),
                     User {
-                        uuid: uuid.clone(),
+                        uid: uid.clone(),
                         username: login_request.username.clone(),
                         hashed_psw: hashed_psw.clone(),
                         salt: salt.clone(),
                         is_admin: false,
                         is_owner: false,
                         permissions: HashMap::new(),
+                        secret: rand_alphanumeric(32),
                     },
                 );
                 Ok(())
-            }
-        }))
+            })
+        })
         .unwrap();
-    Ok(Json(json!(uuid)))
+    Ok(Json(json!(uid)))
 }
 
 pub async fn delete_user(
     Extension(state): Extension<AppState>,
-    Path(uuid): Path<String>,
+    Path(uid): Path<String>,
     AuthBearer(token): AuthBearer,
 ) -> Result<Json<Value>, Error> {
-    if !is_authorized(&token, "", Permission::CanManageUser) {
+    let mut users = state.users.lock().await;
+    let requester = try_auth(&token, users.get_ref()).ok_or(Error {
+        inner: ErrorInner::PermissionDenied,
+        detail: "".to_string(),
+    })?;
+    if !is_authorized(&requester, "", Permission::CanManageUser) {
         return Err(Error {
             inner: ErrorInner::PermissionDenied,
-            detail: "You are not authorized to delete users".to_string(),
+            detail: "You are not authorized to create users".to_string(),
         });
     }
-    let mut users = state.users.lock().await;
     users
         .transform(Box::new(move |v| {
-            v.remove(&uuid);
+            v.remove(&uid);
             Ok(())
         }))
         .unwrap();
@@ -135,14 +142,25 @@ pub async fn delete_user(
 
 pub async fn update_permissions(
     Extension(state): Extension<AppState>,
-    Path(uuid): Path<String>,
+    Path(uid): Path<String>,
     Json(config): Json<Value>,
     AuthBearer(token): AuthBearer,
 ) -> Result<Json<Value>, Error> {
-    if !is_authorized(&token, "", Permission::CanManageUser) {
+    let mut users = state.users.lock().await;
+    let requester = try_auth(&token, users.get_ref()).ok_or(Error {
+        inner: ErrorInner::PermissionDenied,
+        detail: "".to_string(),
+    })?;
+    if !is_authorized(&requester, "", Permission::CanManageUser) {
         return Err(Error {
             inner: ErrorInner::PermissionDenied,
             detail: "You are not authorized to update permissions".to_string(),
+        });
+    }
+    if config["uid"].is_null() {
+        return Err(Error {
+            inner: ErrorInner::MalformedRequest,
+            detail: "Invalid request".to_string(),
         });
     }
     let permissions_update_request: PermissionsUpdateSchema =
@@ -150,10 +168,9 @@ pub async fn update_permissions(
             inner: ErrorInner::MalformedRequest,
             detail: "Invalid request".to_string(),
         }))?;
-    let mut users = state.users.lock().await;
     users
         .transform(Box::new(move |v| {
-            let user = v.get_mut(&uuid).unwrap();
+            let user = v.get_mut(&uid).unwrap();
             user.permissions = permissions_update_request.permissions.clone();
             Ok(())
         }))
@@ -163,32 +180,93 @@ pub async fn update_permissions(
 
 pub async fn get_user_info(
     Extension(state): Extension<AppState>,
-    Path(uuid): Path<String>,
+    Path(uid): Path<String>,
     AuthBearer(token): AuthBearer,
 ) -> Result<Json<Value>, Error> {
-    let user: User = decode_token(token.as_str()).ok_or(Error {
+    let users = state.users.lock().await;
+    let requester = try_auth(&token, users.get_ref()).ok_or(Error {
         inner: ErrorInner::PermissionDenied,
-        detail: "Invalid token".to_string(),
+        detail: "".to_string(),
     })?;
-    if user.uuid != uuid && !is_authorized(&token, "", Permission::CanManageUser) {
+    if requester.uid != uid && !is_authorized(&requester, "", Permission::CanManageUser) {
         return Err(Error {
             inner: ErrorInner::PermissionDenied,
             detail: "You are not authorized to get this user's info".to_string(),
         });
     }
-    let users = state.users.lock().await;
-    let user = users.get_ref().get(&uuid).ok_or(Error {
-        inner: ErrorInner::UserNotFound,
-        detail: "".to_string(),
-    })?;
+    let mut user = users
+        .get_ref()
+        .get(&uid)
+        .ok_or(Error {
+            inner: ErrorInner::UserNotFound,
+            detail: "".to_string(),
+        })?
+        .to_owned();
+    user.salt = "".to_string();
+    user.hashed_psw = "".to_string();
+    user.secret = "".to_string();
     Ok(Json(json!(user)))
+}
+
+pub async fn change_password(
+    Extension(state): Extension<AppState>,
+    Json(config): Json<Value>,
+    AuthBearer(token): AuthBearer,
+) -> Result<Json<Value>, Error> {
+    let mut users = state.users.lock().await;
+    let requester = try_auth(&token, users.get_ref()).ok_or(Error {
+        inner: ErrorInner::PermissionDenied,
+        detail: "Invalid authorization".to_string(),
+    })?;
+    let uid = config
+        .get("uid")
+        .ok_or(Error {
+            inner: ErrorInner::MalformedRequest,
+            detail: "Invalid request, field uid must be present".to_string(),
+        })?
+        .as_str()
+        .ok_or(Error {
+            inner: ErrorInner::MalformedRequest,
+            detail: "Invalid request, field uid must be a string".to_string(),
+        })?
+        .to_string();
+    if requester.uid != uid {
+        return Err(Error {
+            inner: ErrorInner::PermissionDenied,
+            detail: "You are not authorized to change this user's password".to_string(),
+        });
+    }
+    let new_psw = config
+        .get("new_psw")
+        .ok_or(Error {
+            inner: ErrorInner::MalformedRequest,
+            detail: "Invalid request, field new_psw must be present".to_string(),
+        })?
+        .as_str()
+        .ok_or(Error {
+            inner: ErrorInner::MalformedRequest,
+            detail: "Invalid request, field new_psw must be a string".to_string(),
+        })?
+        .to_string();
+    users
+        .transform(Box::new(move |users| {
+            let user = users.get_mut(&uid).unwrap();
+            let mut hasher = Sha3::sha3_256();
+            hasher.input_str(format!("{}{}", user.salt, new_psw).as_str());
+            let hashed_psw = hasher.result_str();
+            user.hashed_psw = hashed_psw;
+            user.secret = rand_alphanumeric(32);
+            Ok(())
+        }))
+        .unwrap();
+    Ok(Json(json!("ok")))
 }
 
 pub async fn login(
     Extension(state): Extension<AppState>,
     AuthBasic((username, psw)): AuthBasic,
 ) -> Result<Json<Value>, Error> {
-    if let Some(user) = state
+    if let Some((_uuid, user)) = state
         .users
         .lock()
         .await
@@ -200,7 +278,7 @@ pub async fn login(
         hasher.input_str(
             format!(
                 "{}{}",
-                user.1.salt,
+                user.salt,
                 psw.ok_or(Error {
                     inner: ErrorInner::MalformedRequest,
                     detail: "".to_string()
@@ -209,13 +287,13 @@ pub async fn login(
             .as_str(),
         );
         let hashed_psw = hasher.result_str();
-        if user.1.hashed_psw != hashed_psw {
+        if user.hashed_psw != hashed_psw {
             return Err(Error {
                 inner: ErrorInner::InvalidPassword,
                 detail: "".to_string(),
             });
         }
-        Ok(Json(json!(create_jwt(&user.1)?)))
+        Ok(Json(json!(create_jwt(&user, &user.secret)?)))
     } else {
         Err(Error {
             inner: ErrorInner::UserNotFound,
--- src/handlers/util.rs
+++ src/handlers/util.rs
@@ -1,56 +1,60 @@
-use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
-use lazy_static::lazy_static;
-use rand::{distributions::Alphanumeric, Rng};
+use std::collections::HashMap;
 
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
 
 use crate::db::{permission::Permission, user::User};
 
 use super::users::Claim;
 
-lazy_static! {
-    pub static ref JWT_SECRET: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(5)
-        .map(char::from)
-        .collect();
-}
-
-pub fn decode_token(token: &str) -> Option<User> {
+fn decode_token(token: &str, jwt_secret : &str) -> Option<String> {
     match decode::<Claim>(
         token,
-        &DecodingKey::from_secret(&*JWT_SECRET.as_bytes()),
+        &DecodingKey::from_secret(jwt_secret.as_bytes()),
         &Validation::new(Algorithm::HS512),
     ) {
-        Ok(t) => Some(t.claims.user),
+        Ok(t) => Some(t.claims.uid),
         Err(_) => None,
     }
 }
 
-pub fn is_authorized(token: &str, instance_uuid: &str, perm: Permission) -> bool {
+fn decode_no_verify(token: &str) -> Option<String> {
+    let mut no_verify = Validation::new(Algorithm::HS512);
+    no_verify.insecure_disable_signature_validation();
     match decode::<Claim>(
         token,
-        &DecodingKey::from_secret(&*JWT_SECRET.as_bytes()),
-        &Validation::new(Algorithm::HS512),
+        &DecodingKey::from_secret("secret".as_bytes()),
+        &no_verify,
     ) {
-        Ok(token) => {
-            let user = token.claims.user;
-            if user.is_owner {
-                return true;
-            }
+        Ok(t) => Some(t.claims.uid),
+        Err(_) => None,
+    }
+}
 
-            match perm {
-                Permission::CanManageUser | Permission::CanAccessMacro => user.is_owner,
-                Permission::CanManagePermission => user.is_admin,
-                _ => {
-                    user.is_admin
-                        || user
-                            .permissions
-                            .get(&perm)
-                            .map(|p| p.contains(instance_uuid))
-                            .unwrap_or(false)
-                }
-            }
+pub fn try_auth(token : &str, users : &HashMap<String, User>) -> Option<User> {
+    let claimed_uid = decode_no_verify(&token)?;
+    let claimed_requester = users.get(&claimed_uid)?;
+    let requester_uid = decode_token(token, &claimed_requester.secret)?;
+    if claimed_uid != requester_uid {
+        return None;
+    }
+    Some(claimed_requester.to_owned())
+}
+
+pub fn is_authorized(user: &User, instance_uuid: &str, perm: Permission) -> bool {
+    if user.is_owner {
+        return true;
+    }
+
+    match perm {
+        Permission::CanManageUser | Permission::CanAccessMacro => user.is_owner,
+        Permission::CanManagePermission => user.is_admin,
+        _ => {
+            user.is_admin
+                || user
+                    .permissions
+                    .get(&perm)
+                    .map(|p| p.contains(instance_uuid))
+                    .unwrap_or(false)
         }
-        Err(_) => false,
     }
 }
