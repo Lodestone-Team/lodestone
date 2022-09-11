53929a09461eea5dd7c566d7f4930fa319a60cae
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-09-11 15:24:26 -0400

    minor UI bug with clipboard text-field

--- src/components/ClipboardTextfield.tsx
+++ src/components/ClipboardTextfield.tsx
@@ -23,7 +23,7 @@ export default function ClipboardTextfield({
 
   return (
     <div
-      className={`${className} hover:cursor-pointer`}
+      className={`${className} hover:cursor-pointer w-fit`}
       onClick={onClickCopy}
       title="Click to Copy"
     > {/* TODO develop custom tooltip component */}
