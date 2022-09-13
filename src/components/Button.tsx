6ffc4f315c3ceaef0ca4af03e658d5693f4fd747
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-09-12 22:53:13 -0400

    minor button UI change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,7 +17,7 @@ export default function Button({
 }) {
   return (
     <button
-      className={`select-none py-1 px-2 rounded-lg bg-gray-600 enabled:outline-gray-400 enabled:outline enabled:outline-1 enabled:text-gray-300 tracking-tight leading-snug font-bold enabled:focus-visible:ring-4 enabled:focus-visible:ring-gray-500 enabled:hover:bg-gray-700 enabled:hover:outline-0 disabled:text-gray-500 ${className}`}
+      className={`select-none py-1 px-2 rounded-lg bg-gray-600 enabled:outline-gray-400 enabled:outline enabled:outline-0 enabled:text-gray-300 tracking-tight leading-snug font-bold enabled:focus-visible:ring-4 enabled:focus-visible:ring-gray-500 enabled:hover:bg-gray-700 enabled:hover:outline-1 disabled:text-gray-500 ${className}`}
       disabled={disabled}
       onClick={onClick}
       type={type}
