95f7555e625269d77af03ad835743b67cff423a3
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-09-26 22:24:28 -0400

    new card design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,7 +20,7 @@ export default function Button({
   disabled = disabled || loading;
   return (
     <button
-      className={`${className} select-none py-1 px-2 rounded-lg bg-gray-600 enabled:outline-gray-400 enabled:outline enabled:outline-0 enabled:text-gray-300 tracking-tight leading-snug font-bold enabled:focus-visible:ring-4 enabled:focus-visible:ring-gray-500 enabled:hover:bg-gray-700 enabled:hover:outline-1 disabled:text-gray-500`}
+      className={`${className} select-none py-1 px-2 rounded-lg bg-gray-600 enabled:outline-gray-400 enabled:outline enabled:outline-0 enabled:text-gray-300 tracking-tight leading-snug font-semibold enabled:focus-visible:ring-4 enabled:focus-visible:ring-gray-500 enabled:hover:bg-gray-700 enabled:hover:outline-1 disabled:text-gray-500`}
       disabled={disabled}
       onClick={onClick}
       type={type}
