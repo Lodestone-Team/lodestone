502e7b18e4bf8f402ed89344b2ab59eaefb639d3
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-10-05 17:43:41 -0400

    added tailwind prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+    semi: true,
+    trailingComma: "es5",
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    plugins: [require('prettier-plugin-tailwindcss')],
+    tailwindConfig: "./tailwind.config.js",
+}
