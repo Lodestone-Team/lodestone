4f880e4f7fe5aded6c856f0323cc17914b895dea
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-10-06 01:20:22 -0400

    minor

--- src/components/Minecraft/MinecraftGeneralCard.tsx
+++ src/components/Minecraft/MinecraftGeneralCard.tsx
@@ -13,37 +13,40 @@ export default function MinecraftGeneralCard({
 }) {
   const queryClient = useQueryClient();
 
+  const portField = (
+    <Textfield
+      label="Port"
+      value={instance.port.toString()}
+      type="number"
+      onSubmit={async (port) => {
+        const numPort = parseInt(port);
+        await axiosPutSingleValue<void>(
+          `/instance/${instance.uuid}/port`,
+          numPort
+        );
+        updateInstance(instance.uuid, queryClient, (oldData) => ({
+          ...oldData,
+          port: numPort,
+        }));
+      }}
+      validate={async (port) => {
+        const numPort = parseInt(port);
+        if (isNaN(numPort)) throw new Error('Port must be a number');
+        if (numPort < 0 || numPort > 65535)
+          throw new Error('Port must be between 0 and 65535');
+        const result = await axiosWrapper<boolean>({
+          method: 'get',
+          url: `/check/port/${numPort}`,
+        });
+        if (result) throw new Error('Port not available');
+      }}
+    />
+  );
+
   return (
     <DashboardCard>
       <h1 className="font-bold text-medium"> General Settings </h1>
-      <div className="flex flex-row">
-        <Textfield
-          label="Port"
-          value={instance.port.toString()}
-          onSubmit={async (port) => {
-            const numPort = parseInt(port);
-            await axiosPutSingleValue<void>(
-              `/instance/${instance.uuid}/port`,
-              numPort
-            );
-            updateInstance(instance.uuid, queryClient, (oldData) => ({
-              ...oldData,
-              port: numPort,
-            }));
-          }}
-          validate={async (port) => {
-            const numPort = parseInt(port);
-            if (isNaN(numPort)) throw new Error('Port must be a number');
-            if (numPort < 0 || numPort > 65535)
-              throw new Error('Port must be between 0 and 65535');
-            const result = await axiosWrapper<boolean>({
-              method: 'get',
-              url: `/check/port/${numPort}`,
-            });
-            if (result) throw new Error('Port not available');
-          }}
-        />
-      </div>
+      <div className="flex flex-row">{portField}</div>
     </DashboardCard>
   );
 }
