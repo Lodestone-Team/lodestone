b514f0d42223265a848ebb85e55164e9b0ab1f43
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-10-08 22:23:56 -0400

    improved dashboard card styles

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from 'react';
 
-export default function DashboardCard({ children }: { children: ReactNode }) {
+export default function DashboardCard({
+  children,
+  className = '',
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="flex flex-col items-start w-full gap-4 px-10 pt-8 pb-10 bg-gray-900 rounded-2xl justify-evenly h-fit">
+    <div
+      className={`flex h-fit w-full flex-col items-start justify-evenly gap-8 rounded-2xl bg-gray-900 px-10 pt-8 pb-10 ${className}`}
+    >
       {children}
     </div>
   );
