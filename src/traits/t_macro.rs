6766138078bd70be9308d3771d8c8fc1489f6f64
Peter Jiang <2892084432j@gmail.com>
2022-10-25 00:43:47 -0400

    format

--- src/traits/t_macro.rs
+++ src/traits/t_macro.rs
@@ -1,4 +1,9 @@
+
+
 use async_trait::async_trait;
+
+
+
 
 use super::MaybeUnsupported;
 
--- src/traits/t_server.rs
+++ src/traits/t_server.rs
@@ -1,5 +1,8 @@
+
+
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
+
 use ts_rs::TS;
 
 use super::MaybeUnsupported;
