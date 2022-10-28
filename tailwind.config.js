cf28f6d2fc2dabaf85a2e0a63e7fb3d174f8d173
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-10-27 20:09:49 -0400

    updated to next.js 13, no migration yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
