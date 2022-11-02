f0aa1b99c8ed9c860f6c78fa03edfcf58ddbef53
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2022-11-02 15:26:04 -0400

    Integrated tauri and client (#26)
    Co-authored-by: Peter Jiang <47200772+CheatCod@users.noreply.github.com>

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::thread;
+
+#[tokio::main]
+async fn main() {
+  tokio::spawn(async {
+      lodestone_client::run().await;
+  });
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+}
