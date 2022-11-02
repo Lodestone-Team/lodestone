811b6a0b096a2ba1cedbcc80571beab0007dd713
Peter Jiang <47200772+CheatCod@users.noreply.github.com>
2022-11-02 15:20:55 -0400

    integrate client

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,17 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+use std::thread;
+
+#[tokio::main]
+async fn main() {
+  tokio::spawn(async {
+      lodestone_client::run().await;
+  });
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
 }
