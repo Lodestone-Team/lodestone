9fa272b19e00a5251c484fefbb6b9479a4f9dcd4
Peter Jiang <2892084432j@gmail.com>
2022-11-07 12:31:47 -0500

    update port allocator

--- src/port_allocator.rs
+++ src/port_allocator.rs
@@ -1,7 +1,15 @@
 use std::collections::HashSet;
 
+use serde::{Serialize, Deserialize};
+
 pub struct PortAllocator {
     allocated_ports: HashSet<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+pub struct PortStatus {
+    pub is_in_use: bool,
+    pub is_allocated: bool,
 }
 
 impl PortAllocator {
@@ -13,7 +21,7 @@ impl PortAllocator {
         if self.allocated_ports.contains(&start_port) {
             let mut new_port = start_port + 1;
             while self.allocated_ports.contains(&new_port)
-                && port_scanner::local_port_available(new_port as u16)
+                || !port_scanner::local_port_available(new_port as u16)
             {
                 new_port += 1;
             }
@@ -25,8 +33,11 @@ impl PortAllocator {
         }
     }
 
-    pub fn is_port_in_use(&self, port: u32) -> bool {
-        self.allocated_ports.contains(&port) || !port_scanner::local_port_available(port as u16)
+    pub fn port_status(&self, port: u32) -> PortStatus {
+        PortStatus {
+            is_in_use: !port_scanner::local_port_available(port as u16),
+            is_allocated: self.allocated_ports.contains(&port),
+        }
     }
 
     pub fn add_port(&mut self, port: u32) {
