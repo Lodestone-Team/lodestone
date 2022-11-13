376bc21ea227323db2234730cfffe2a9431a2ce8
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-11-13 15:32:36 -0500

    fixed instance card storybook

--- src/components/InstanceCard.stories.tsx
+++ src/components/InstanceCard.stories.tsx
@@ -1,5 +1,39 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { setupWorker, rest } from 'msw';
 import InstanceCard from './InstanceCard';
+
+const mockedUserInfo = {
+  uid: '109bdc1d-f7ab-4186-a9c2-d1a9aaf8d937',
+  username: 'owner',
+  is_owner: true,
+  is_admin: false,
+  permissions: {
+    can_view_instance: [],
+    can_start_instance: [],
+    can_stop_instance: [],
+    can_access_instance_console: [],
+    can_access_instance_setting: [],
+    can_read_instance_resource: [],
+    can_write_instance_resource: [],
+    can_access_instance_macro: [],
+    can_read_instance_file: [],
+    can_write_instance_file: [],
+    can_create_instance: false,
+    can_delete_instance: false,
+    can_read_global_file: false,
+    can_write_global_file: false,
+    can_manage_permission: false,
+  },
+};
+
+const worker = setupWorker(
+  rest.get('/user/info', (req, res, ctx) => {
+    return res(ctx.json(mockedUserInfo));
+  })
+);
+
+worker.start();
 
 export default {
   title: 'Components/InstanceCard',
@@ -19,9 +53,11 @@ export default {
 } as ComponentMeta<typeof InstanceCard>;
 
 const Template: ComponentStory<typeof InstanceCard> = (args) => (
-  <div className="flex flex-col child:w-full">
-    <InstanceCard {...args} />
-  </div>
+  <QueryClientProvider client={new QueryClient()}>
+    <div className="flex flex-col child:w-full w-60">
+      <InstanceCard {...args} />
+    </div>
+  </QueryClientProvider>
 );
 
 export const Default = Template.bind({});
