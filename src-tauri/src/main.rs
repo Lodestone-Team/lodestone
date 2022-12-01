e9e7b9412dc043e0de1933a61963874f62ebcda8
Peter Jiang <2892084432j@gmail.com>
2022-11-30 21:53:01 -0500

    add tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,43 @@
     windows_subsystem = "windows"
 )]
 
+use lodestone_client::AppState;
+
+use lodestone_client::Error;
+
+use lodestone_client::tauri_export::is_owner_account_present;
+
+#[tauri::command]
+async fn is_setup(state: tauri::State<'_, AppState>) -> Result<bool, ()> {
+    Ok(is_owner_account_present(state.inner()).await)
+}
+
+#[tauri::command]
+async fn setup_owner_account(
+    state: tauri::State<'_, AppState>,
+    username: String,
+    password: String,
+) -> Result<(), Error> {
+    lodestone_client::tauri_export::setup_owner_account(state.inner(), username, password).await
+}
+
+#[tauri::command]
+async fn get_owner_jwt(state: tauri::State<'_, AppState>) -> Result<String, ()> {
+    lodestone_client::tauri_export::get_owner_jwt(state.inner())
+        .await
+        .ok_or(())
+}
 
 #[tokio::main]
 async fn main() {
-  tokio::spawn(async {
-      lodestone_client::run().await;
-  });
+    let app_state = lodestone_client::run().await;
     tauri::Builder::default()
+        .manage(app_state)
+        .invoke_handler(tauri::generate_handler![
+            is_setup,
+            setup_owner_account,
+            get_owner_jwt
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
 }
