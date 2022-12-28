fec098b71c04493db1e08d4ac432ff5fd1de72e0
Joey Wang <joeywangzr@gmail.com>
2022-12-28 15:51:40 -0700

    updated selectfield dropdown

--- src/components/Atoms/Form/SelectField.tsx
+++ src/components/Atoms/Form/SelectField.tsx
@@ -4,7 +4,7 @@ import { FieldHookConfig, useField } from 'formik';
 import { Listbox } from '@headlessui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BeatLoader from 'react-spinners/BeatLoader';
-import { faCaretDown, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faSort, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export type SelectFieldProps = FieldHookConfig<string> & {
   label?: string;
@@ -52,10 +52,36 @@ export default function SelectField(props: SelectFieldProps) {
   ) : (
     <FontAwesomeIcon
       key="icon"
-      icon={faCaretDown}
+      icon={faSort}
       className="w-4 text-gray-faded/30 group-enabled:group-hover:cursor-pointer group-enabled:group-hover:text-gray-500"
     />
   );
+
+  const itemActionIcon = loadingVisual ? (
+    <BeatLoader
+      key="loading"
+      size="0.25rem"
+      cssOverride={{
+        width: '2rem',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: `0 -0.5rem`,
+      }}
+    />
+  ) : (
+    <FontAwesomeIcon
+      key="icon"
+      icon={faTrashAlt}
+      className={
+        'w-4 text-gray-faded/30 cursor-pointer hover:text-gray-500'
+      }
+    />
+  );
+
+  function itemAction() {
+    // pass
+  }
 
   return (
     <div
@@ -81,7 +107,7 @@ export default function SelectField(props: SelectFieldProps) {
           disabled={disabledVisual}
         >
           <Listbox.Button
-            className={`input-base group min-h-[1em] w-full ${
+            className={`ui-open:bg-gray-700 ui-not-open:bg-gray-900 input-base group min-h-[1em] w-full ${
               errorText ? 'border-error' : 'border-normal'
             } ${selectedValue ? 'text-gray-300' : 'text-gray-500'}`}
           >
@@ -91,35 +117,29 @@ export default function SelectField(props: SelectFieldProps) {
                 <div className="flex flex-row gap-2">{icon}</div>
               </div>
             </>
+            <div className="pointer-events-none absolute top-0 right-0 flex h-full flex-row items-center justify-end py-1.5 px-3">
+              <div className="flex flex-row gap-2">{icon}</div>
+            </div>
           </Listbox.Button>
+
           <Listbox.Options
-            className={`input-base border-normal absolute z-50 mt-2 max-h-60 w-full overflow-auto p-0 py-1 shadow-md`}
+            className={`input-base border-normal absolute z-50 mt-2 max-h-60 w-full overflow-auto p-0 py-2 shadow-md`}
           >
             {options.map((option) => (
               <Listbox.Option
                 key={option}
                 value={option}
-                className={({ active }) => {
-                  return `relative cursor-default select-none py-2 pl-8 pr-4 text-gray-300 ${
-                    active ? 'bg-gray-800' : 'bg-gray-900'
-                  }`;
-                }}
+                className="border border-gray-400/30 relative cursor-default select-none py-2 pl-3 pr-4 text-gray-300 ui-selected:font-medium ui-not-selected:font-normal ui-selected:ui-active:bg-gray-600 ui-selected:ui-not-active:bg-gray-600 ui-not-selected:ui-active:bg-gray-800 ui-not-selected:ui-not-active:bg-gray-900"
               >
-                {({ selected }) => (
-                  <>
-                    <span
-                      className={`block truncate ${
-                        selected ? 'font-medium' : 'font-normal'
-                      }`}
-                    >
-                      {option}
-                    </span>
-                    {selected && (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-2.5 text-green-200">
-                        <FontAwesomeIcon icon={faCheck} className="h-4 w-4" />
+                {({ active }) => (
+                    <div className="flex flex-row justify-between">
+                      <span
+                        className="block truncate pr-1"
+                      >
+                        {option}
                       </span>
-                    )}
-                  </>
+                      <div onClick={itemAction} className="right-3 absolute">{active && itemActionIcon}</div>
+                    </div>
                 )}
               </Listbox.Option>
             ))}
