0bdbe613ec6921b26656f4ae1e2e98cef96d4a4d
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-12-31 02:00:27 -0500

    Removed enter transition in dropdowns to match comboxfield

--- src/components/Atoms/Form/SelectField.tsx
+++ src/components/Atoms/Form/SelectField.tsx
@@ -119,9 +119,6 @@ export default function SelectField(props: SelectFieldProps) {
 
           <Transition
             as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 -translate-y-1"
-            enterTo="opacity-100 translate-y-0"
             leave="transition ease-in duration-150"
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 -translate-y-1"
