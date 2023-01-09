d711e7f064c5fb09cf9e6af822db6c1412335e41
Peter <2892084432j@gmail.com>
2023-01-09 01:08:59 -0500

    binding change

--- bindings/EventQuery.ts
+++ bindings/EventQuery.ts
@@ -3,6 +3,8 @@ import type { EventLevel } from "./EventLevel";
 import type { EventType } from "./EventType";
 import type { InstanceEventKind } from "./InstanceEventKind";
 import type { InstanceUuid } from "./InstanceUuid";
+import type { TimeRange } from "./TimeRange";
 import type { UserEventKind } from "./UserEventKind";
+import type { UserId } from "./UserId";
 
-export interface EventQuery { event_levels: Array<EventLevel> | null, event_types: Array<EventType> | null, instance_event_types: Array<InstanceEventKind> | null, user_event_types: Array<UserEventKind> | null, event_instance_ids: Array<InstanceUuid> | null, bearer_token: string | null, }
+export interface EventQuery { event_levels: Array<EventLevel> | null, event_types: Array<EventType> | null, instance_event_types: Array<InstanceEventKind> | null, user_event_types: Array<UserEventKind> | null, event_user_ids: Array<UserId> | null, event_instance_ids: Array<InstanceUuid> | null, bearer_token: string | null, time_range: TimeRange | null, }
