e865a708501a1ad1a5820ae5f7b3afe43e2f0282
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2023-01-13 23:55:42 -0500

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,22 @@ class MyDocument extends Document {
             href="https://api.fontshare.com/v2/css?f[]=chillax@1&f[]=satoshi@1,2&f[]=jet-brains-mono@1,2&f[]=clash-grotesk@1&display=swap"
             rel="stylesheet"
           />
+          {/* <!-- Google tag (gtag.js) --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-LZQ3VZ6N26"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LZQ3VZ6N26');`,
+            }}
+          />
         </Head>
+
         <body>
           <Main />
           <NextScript />
