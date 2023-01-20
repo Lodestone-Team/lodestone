eb78e44a05f178df847eef84654d36f2ed3f8fe2
Arcslogger <wilbur.zhaang@gmail.com>
2023-01-19 23:39:26 -0500

    more merge fixes

--- src/pages/login/FirstTime.tsx
+++ src/pages/login/FirstTime.tsx
@@ -2,11 +2,10 @@ import Button from 'components/Atoms/Button';
 import { useContext } from 'react';
 import { DEFAULT_LOCAL_CORE } from 'utils/util';
 import { BrowserLocationContext } from 'data/BrowserLocationContext';
-import { useQueryClient } from '@tanstack/react-query';
-import { useGlobalSettings } from 'data/GlobalSettings';
 import { LodestoneContext } from 'data/LodestoneContext';
 import { useDocumentTitle, useEffectOnce } from 'usehooks-ts';
 import { tauri } from 'utils/tauriUtil';
+import packageinfo from '../../../package.json';
 
 const FirstTime = () => {
   useDocumentTitle('Welcome to Lodestone');
@@ -36,16 +35,35 @@ const FirstTime = () => {
   });
 
   return (
-    <div className="flex w-[640px] max-w-full flex-col items-stretch justify-center gap-16 rounded-2xl bg-gray-850 px-12 py-16 transition-dimensions @container">
-      <div className="text flex flex-col items-start">
-        <img src="/logo.svg" alt="logo" className="h-fit w-fit" />
-        <h1 className="font-title text-h1 font-bold tracking-medium text-gray-300">
-          Welcome to Lodestone
-        </h1>
-        <h2 className="text-medium font-medium tracking-medium text-white/75">
-          Learn more about Lodestone and any known issues on our {" "}
+    <div className="flex w-[680px] max-w-full flex-col items-stretch justify-center gap-16 transition-dimensions @container">
+      <div className="text flex flex-col items-start gap-4">
+        <div className="flex w-full flex-row items-center gap-4">
+          <img src="/logo.svg" alt="logo" className="h-8" />
+          <Button
+            label="Wiki & FAQ"
+            onClick={() => {
+              window.open(
+                'https://github.com/Lodestone-Team/lodestone/wiki',
+                '_self'
+              );
+            }}
+          />
+        </div>
+
+        <div className="flex flex-row items-start gap-4">
+          <h1 className="font-title text-title font-bold tracking-medium text-gray-300">
+            Welcome to Lodestone!
+          </h1>
+          <span className="mt-3 text-right text-small font-medium tracking-medium text-green-200">
+            VERSION {packageinfo.version}
+          </span>
+        </div>
+
+        <p className="rounded-md border border-red-200 bg-red-faded/25 p-2 text-medium font-medium tracking-medium text-white">
+          Lodestone requires mixed/insecure content to be allowed in your
+          browser. Learn how to do it{' '}
           <a
-            href="https://github.com/Lodestone-Team/lodestone/wiki"
+            href="https://experienceleague.adobe.com/docs/target/using/experiences/vec/troubleshoot-composer/mixed-content.html?lang=en"
             target="_blank"
             rel="noreferrer"
             className="text-blue-200 underline hover:text-blue-300"
@@ -57,9 +75,9 @@ const FirstTime = () => {
         <p className="text-medium font-medium tracking-medium text-white">
           Our product is still in its beta release cycle. Browser support is
           limited and bugs are expected. You can check known issues and report
-          any new ones on our {' '}
+          any new ones on our{' '}
           <a
-            href="https://github.com/Lodestone-Team/lodestone"
+            href="https://github.com/Lodestone-Team/lodestone/issues"
             target="_blank"
             rel="noreferrer"
             className="text-blue-200 underline hover:text-blue-300"
@@ -71,7 +89,6 @@ const FirstTime = () => {
 
       <div className="flex flex-row items-baseline gap-4">
         <Button
-          className="flex-1"
           label="Download Lodestone Core"
           onClick={() => {
             window.open(
@@ -84,7 +101,6 @@ const FirstTime = () => {
         />
 
         <Button
-          className="flex-1"
           label="Connect to existing Core"
           onClick={() => setPathname('/login/core/new')}
           intention="primary"
