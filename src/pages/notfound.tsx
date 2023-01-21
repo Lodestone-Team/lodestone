749dad86b1177888fa9975821c432d70e3d79410
Ynng <44907675+Ynng@users.noreply.github.com>
2023-01-21 18:25:23 -0500

    Revert "unrealted: changed h-screen to h-full incase of top/bottom banners"
    This reverts commit 1c0ec0fb9f952cb407aef2d2615974a4840a6de7.

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -4,7 +4,7 @@ import { useDocumentTitle } from 'usehooks-ts';
 export default function NotFound() {
   useDocumentTitle('404 - Lodestone');
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-gray-900">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-gray-900">
       <h1 className="font-title text-6xlarge font-bold">404</h1>
       <h2 className="text-2xlarge font-bold">Page Not Found</h2>
       <Button
