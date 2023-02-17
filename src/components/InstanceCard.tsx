deabb618c9ee3c98e46141b9406389a73bf74837
NicholasLin718 <nlin311@gmail.com>
2023-02-17 17:57:55 -0500

    added dropdown for instance state

--- src/components/InstanceCard.tsx
+++ src/components/InstanceCard.tsx
@@ -4,7 +4,7 @@ import {
   stateToLabelColor,
 } from 'utils/util';
 import Label from './Atoms/Label';
-import { useContext, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import axios from 'axios';
 import { InstanceState } from 'bindings/InstanceState';
 import { InstanceInfo } from 'bindings/InstanceInfo';
@@ -13,12 +13,24 @@ import GameIcon from './Atoms/GameIcon';
 import clsx from 'clsx';
 import { toast } from 'react-toastify';
 import useAnalyticsEventTracker from 'utils/hooks';
-import { faPowerOff, faRefresh } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowRightArrowLeft,
+  faArrowRotateBackward,
+  faCaretDown,
+  faPowerOff,
+  faRefresh,
+  faRightFromBracket,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import IconButton from './Atoms/IconButton';
 import ClipboardTextfield from './ClipboardTextfield';
 import { useGlobalSettings } from 'data/GlobalSettings';
 import { LodestoneContext } from 'data/LodestoneContext';
 import Tooltip from 'rc-tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Menu, Transition } from '@headlessui/react';
+import Avatar from 'boring-avatars';
+import Button from './Atoms/Button';
 
 // // for the css style of the double border when focused
 // const stateToBorderMap: { [key in InstanceState]: string[] } = {
@@ -155,10 +167,6 @@ export default function InstanceCard({
           className="h-4 w-4"
         />
         <p className="grow truncate">{name}</p>
-        <IconButton
-          icon={faRefresh}
-          onClick={() => state === 'Running' && restartButtonOnClick()}
-        />
         <Tooltip
           showArrow={false}
           overlay={<span>{actionMessage}</span>}
@@ -166,7 +174,55 @@ export default function InstanceCard({
           trigger={['hover']}
           mouseEnterDelay={0.2}
         >
-          <IconButton icon={faPowerOff} onClick={buttonOnClick} />
+          {state !== 'Running' ? (
+            <IconButton icon={faPowerOff} onClick={buttonOnClick} />
+          ) : (
+            <Menu as="div" className="relative">
+              <Menu.Button as={IconButton} icon={faPowerOff}></Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 -translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-1"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-1.5 origin-top-left divide-y divide-gray-faded/30 rounded border border-gray-faded/30 bg-gray-800 drop-shadow-md focus:outline-none">
+                  <div className="py-2 px-1.5">
+                    <Menu.Item>
+                      {({ disabled }) => (
+                        <Button
+                          className="w-full whitespace-nowrap"
+                          label={'Stop'}
+                          iconRight={faPowerOff}
+                          onClick={() =>
+                            state === 'Running' && restartButtonOnClick()
+                          }
+                          align="end"
+                          disabled={disabled}
+                          variant="text"
+                        />
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ disabled }) => (
+                        <Button
+                          className="w-full flex-nowrap whitespace-nowrap"
+                          label="Restart"
+                          iconRight={faArrowRotateBackward}
+                          align="end"
+                          disabled={disabled}
+                          onClick={buttonOnClick}
+                          variant="text"
+                        />
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          )}
         </Tooltip>
       </div>
       <div
