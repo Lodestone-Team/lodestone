047baf711eb82c6075f69c600a520d99322ecc82
NicholasLin718 <nlin311@gmail.com>
2023-03-03 03:11:53 -0500

    styled select game page

--- src/components/Minecraft/Create/MinecraftGameForm.tsx
+++ src/components/Minecraft/Create/MinecraftGameForm.tsx
@@ -1,91 +1,63 @@
 import { RadioGroup } from '@headlessui/react';
+import { HandlerGameType } from 'bindings/HandlerGameType';
+import { GameType } from 'bindings/InstanceInfo';
 import clsx from 'clsx';
 import InputField from 'components/Atoms/Form/InputField';
 import RadioField from 'components/Atoms/Form/RadioField';
+import Spinner from 'components/DashboardLayout/Spinner';
 import {
   InstanceGameTypes,
   SetupInstanceManifest,
 } from 'data/InstanceGameTypes';
 import SelectGameCard from './SelectGameCard';
-export default function MinecraftAdvancedForm({ gameType, setGameType }: any) {
-  // // const { gameType, setGameType } = props;
+import { gameTypeInfoFromHandlerType } from 'data/GameTypeMappings';
+export default function MinecraftAdvancedForm({
+  gameType,
+  setGameType,
+  className,
+}: {
+  gameType: HandlerGameType;
+  setGameType: (gameType: HandlerGameType) => void;
+  className?: string;
+}) {
   const { data: game_types, isLoading, error } = InstanceGameTypes();
-  // console.log(gametypes);
-  // const {
-  //   data: setup_manifest,
-  //   isLoading,
-  //   error,
-  // } = SetupInstanceManifest('MinecraftPaper');
-  // console.log(setup_manifest);
-  // const allGames = [
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftVanilla',
-  //     description: 'Vanilla Minecraft',
-  //   },
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftForge',
-  //     description: 'Minecraft with Forge',
-  //   },
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftFabric',
-  //     description: 'Minecraft with Fabric',
-  //   },
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftPaper',
-  //     description: 'Minecraft with Paper',
-  //   },
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftSpigot',
-  //     description: 'Minecraft with Spigot',
-  //   },
-  //   {
-  //     game: 'Minecraft',
-  //     game_type: 'MinecraftBedrock',
-  //     description: 'Minecraft Bedrock',
-  //   },
-  // ];
-  // console.log(gameType);
-  // console.log(allGames[0].game_type);
   if (!game_types || isLoading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
+
+  const game_typesDuped = [game_types].flat();
   return (
-    <>
-      <h1 className="text-h1 font-bold tracking-medium text-gray-300">
+    <div className={className}>
+      <p className="text-left text-h2 font-bold tracking-medium text-gray-300">
         Select Game
-      </h1>
-      <p>
-        Choose your game type.
-        <br />
       </p>
-      <div className="flex flex-wrap">
-        {game_types.map((game) => (
-          <div
-            key={game}
-            className="w-full p-4 hover:cursor-pointer sm:w-1/2"
-            onClick={() => setGameType(game)}
-          >
-            <SelectGameCard
-              title={game}
-              description={game}
-              className={clsx(
-                game === gameType && 'border-2 border-red-300',
-                'flex h-28 items-center justify-center rounded-lg bg-blue-300'
-              )}
-            />
-            {/* <div
-              className={}
+      <p className="text-left text-medium font-bold italic leading-5 tracking-medium text-white/50">
+        What will your server be used for?
+      </p>
+      <div className="mt-9 box-border grid h-full grid-cols-2 gap-9">
+        {game_typesDuped.map((game) => {
+          const { title, description, game_type } =
+            gameTypeInfoFromHandlerType[game];
+          console.log(gameTypeInfoFromHandlerType[game]);
+          return (
+            <div
+              key={game}
+              className="w-full hover:cursor-pointer"
+              onClick={() => setGameType(game)}
             >
-              <span>{game.game_type}</span>
-            </div> */}
-          </div>
-        ))}
+              <SelectGameCard
+                title={title}
+                description={description}
+                game_type={game_type}
+                className={clsx(
+                  'h-28 rounded-lg bg-gray-800 p-4 outline outline-1 outline-fade-700/10',
+                  game === gameType && 'border-2 border-red-300'
+                )}
+              />
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Minecraft/Create/SelectGameCard.tsx
+++ src/components/Minecraft/Create/SelectGameCard.tsx
@@ -1,25 +1,29 @@
 import GameIcon from 'components/Atoms/GameIcon';
 import React from 'react';
 import clsx from 'clsx';
+import { GameType } from 'bindings/InstanceInfo';
 const SelectGameCard = ({
   title,
   description,
+  game_type,
   className,
 }: {
   title: string;
   description: string;
+  game_type: GameType;
   className?: string;
 }) => {
   return (
-    <div className={clsx('', className)}>
-      <span>
-        {title}
-        {/* <GameIcon
-          game_type={'minecraft'}
-          game_flavour={'vanilla'}
-          className="h-4 w-4"
-        /> */}
-      </span>
+    <div className={clsx(className, 'text-left font-sans tracking-medium')}>
+      <div className="relative">
+        <GameIcon game_type={game_type} className="absolute h-6 w-6" />
+        <div className="ml-8 text-h3 font-extrabold leading-6 text-gray-300">
+          {title}
+        </div>
+      </div>
+      <div className=" text-medium font-mediumbold italic leading-5 text-white/50">
+        {description}
+      </div>
     </div>
   );
 };
