d4b514005914be6a63c32b4623066b93e756d776
Hanmin Kim <hanminkim123@gmail.com>
2023-03-14 19:43:54 -0400

    fix scrollbar issue on Chrome

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -21,10 +21,9 @@ interface CardProps {
 
 export function Table({rows, columns, className}: CardProps) {
     return (
-        <div className={cn("flex h-fit max-h-full w-full flex-col overflow-x-auto bg-gray-875", className)}>
-            <div className="flex h-fit min-h-fit w-fit flex-col">
-                <div
-                    className="flex h-6 min-h-[24px] items-center gap-2.5 self-stretch overflow-x-hidden border-b border-b-fade-700 px-3">
+        <div className={cn("overflow-overlay flex h-fit max-h-full w-full flex-col overflow-x-auto bg-gray-875", className)}>
+            <div className="flex h-fit w-max min-w-full flex-col">
+                <div className="flex h-6 min-h-[24px] items-center gap-2.5 self-stretch overflow-x-hidden border-b border-b-fade-700 pl-3">
                     {columns.map((column, cIndex) => {
                             return (
                                 <div className="" key={cIndex} style={{width: column.width}}>
@@ -35,7 +34,7 @@ export function Table({rows, columns, className}: CardProps) {
                     )}
                 </div>
 
-                <div className={`flex flex-col overflow-x-hidden overflow-y-scroll`}>
+                <div className={`flex scroll-pr-0 flex-col overflow-x-hidden overflow-y-scroll`}>
                     {rows.map((row, index) => {
 
                         return (
