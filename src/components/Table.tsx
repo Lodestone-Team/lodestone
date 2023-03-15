5e682548bc4c3678b0f39609b43711542d7dc155
Hanmin Kim <hanminkim123@gmail.com>
2023-03-14 22:00:58 -0400

    general code cleanup for table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {cn} from "../utils/util";
+import clsx from "clsx";
 
 
 export interface TableColumn {
@@ -23,14 +24,13 @@ export function Table({rows, columns, className}: CardProps) {
     return (
         <div className={cn("flex h-fit max-h-full w-full flex-col overflow-auto bg-gray-875", className)}>
             <div className="flex h-fit w-max min-w-full flex-col">
-                <div className="sticky top-0 flex h-6 min-h-[24px] items-center gap-2.5 self-stretch overflow-x-hidden border-b border-b-fade-700 bg-gray-875 px-3">
-                    {columns.map((column, cIndex) => {
-                            return (
-                                <div className="" key={cIndex} style={{width: column.width}}>
-                                    <span className="text-small font-bold tracking-medium">{column.headerName}</span>
-                                </div>
-                            )
-                        }
+                <div
+                    className="sticky top-0 flex h-6 items-center gap-2.5 self-stretch overflow-hidden border-b border-b-fade-700 bg-gray-875 px-3">
+                    {columns.map((column, cIndex) => (
+                            <div key={cIndex} style={{width: column.width}}>
+                                <span className="text-small font-bold tracking-medium">{column.headerName}</span>
+                            </div>
+                        )
                     )}
                 </div>
 
@@ -38,16 +38,14 @@ export function Table({rows, columns, className}: CardProps) {
 
                     return (
                         <div
-                            className={"flex flex-row items-center px-3 py-2.5 gap-2.5 h-10 border-b border-b-fade-700 " + (index % 2 === 1 ? "bg-gray-850" : "")}
+                            className={clsx("flex h-10 flex-row items-center gap-2.5 border-b border-b-fade-700 px-3 py-2.5", index % 2 === 1 && "bg-gray-850")}
                             key={index}>
-                            {columns.map((column, cIndex) => {
-                                    return (
-                                        <div key={cIndex} style={{width: column.width}}>
-                                                <span
-                                                    className={cn("whitespace-nowrap text-small tracking-medium text-white/50", column.className)}>{row[column.field]}</span>
-                                        </div>
-                                    )
-                                }
+                            {columns.map((column, cIndex) => (
+                                    <div key={cIndex} style={{width: column.width}}>
+                                        <span
+                                            className={cn("whitespace-nowrap text-small tracking-medium text-white/50", column.className)}>{row[column.field]}</span>
+                                    </div>
+                                )
                             )}
                         </div>
                     )
