10b803784186aadbb9dc9c5da77b9486d1945bf4
Hanmin Kim <hanminkim123@gmail.com>
2023-03-14 23:31:16 -0400

    use html table for Table

--- src/components/Table.stories.tsx
+++ src/components/Table.stories.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
 
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 import {Table, TableColumn, TableRow} from "./Table";
+import {cn} from "../utils/util";
 
 const mockTableColumns: TableColumn[] = [
-    { field: "name", headerName: "TASK NAME", width: 200, className: "font-mono text-white" },
-    { field: "start", headerName: "CREATED", width: 200 },
-    { field: "finish", headerName: "FINISHED", width: 200 },
-    { field: "pId", headerName: "PROCESS ID", width: 200 },
+    {field: "name", headerName: "TASK NAME", className: "font-mono text-white"},
+    {field: "start", headerName: "CREATED"},
+    {field: "finish", headerName: "FINISHED"},
+    {field: "pId", headerName: "PROCESS ID"},
 ]
 
 const mockTableRows: TableRow[] = [
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
-    { name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890 },
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
+    {name: "Hello.js", start: "Jan 4 2023, 09:37", finish: "Jan 4 2023, 09:37", pId: 1234567890},
 ]
 
 export default {
-    title: 'Table',
+    title: 'library/Table',
     component: Table,
 } as ComponentMeta<typeof Table>;
 
@@ -38,12 +39,24 @@ const Template: ComponentStory<typeof Table> = (args) =>
     (
         <Table {...args}/>
     )
+const TemplateDiv: ComponentStory<typeof Table> = (args) =>
+    (
+        <div className={cn("flex h-fit max-h-full w-full flex-col overflow-auto bg-gray-875", args.className)}>
+            <Table {...args}/>
+        </div>
+    )
 
 export const Primary = Template.bind({});
-export const Clip = Template.bind({});
+export const Minimal = Template.bind({});
+export const Clip = TemplateDiv.bind({});
 
 
 Primary.args = {
+    columns: mockTableColumns,
+    rows: mockTableRows.slice(0, 5),
+    className: "w-[600px]",
+}
+Minimal.args = {
     columns: mockTableColumns,
     rows: mockTableRows.slice(0, 5),
     className: "w-full",
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,7 +6,6 @@ import clsx from "clsx";
 export interface TableColumn {
     field: string;
     headerName: string;
-    width: number;
     className?: string;
 }
 
@@ -22,35 +21,39 @@ interface CardProps {
 
 export function Table({rows, columns, className}: CardProps) {
     return (
-        <div className={cn("flex h-fit max-h-full w-full flex-col overflow-auto bg-gray-875", className)}>
-            <div className="flex h-fit w-max min-w-full flex-col">
-                <div
-                    className="sticky top-0 flex h-6 items-center gap-2.5 self-stretch overflow-hidden border-b border-b-fade-700 bg-gray-875 px-3">
-                    {columns.map((column, cIndex) => (
-                            <div key={cIndex} style={{width: column.width}}>
-                                <span className="text-small font-bold tracking-medium">{column.headerName}</span>
-                            </div>
-                        )
-                    )}
-                </div>
-
-                {rows.map((row, index) => {
-
-                    return (
-                        <div
-                            className={clsx("flex h-10 flex-row items-center gap-2.5 border-b border-b-fade-700 px-3 py-2.5", index % 2 === 1 && "bg-gray-850")}
-                            key={index}>
-                            {columns.map((column, cIndex) => (
-                                    <div key={cIndex} style={{width: column.width}}>
-                                        <span
-                                            className={cn("whitespace-nowrap text-small tracking-medium text-white/50", column.className)}>{row[column.field]}</span>
-                                    </div>
-                                )
-                            )}
-                        </div>
+        <table className={cn("table-auto whitespace-nowrap bg-gray-875 text-left tracking-medium", className)}>
+            <thead className="h-6 border-b border-b-fade-700 bg-gray-875">
+            <tr>
+                {columns.map((column, cIndex) => (
+                        <th key={cIndex}
+                            className="sticky top-0 bg-gray-875 px-3 text-small font-bold">
+                            {column.headerName}
+                        </th>
                     )
-                })}
-            </div>
-        </div>
+                )}
+            </tr>
+            </thead>
+            <tbody>
+            {rows.map((row, index) => {
+
+                return (
+                    <tr
+                        className={clsx("h-10 border-b border-b-fade-700", index % 2 === 1 && "bg-gray-850")}
+                        key={index}>
+                        {columns.map((column, cIndex) => (
+                                <td key={cIndex}
+                                    className={cn("px-3 text-left text-white/50", column.className)}>
+                                    {row[column.field]}
+                                </td>
+                            )
+                        )}
+                    </tr>
+                )
+            })}
+
+
+            </tbody>
+
+        </table>
     );
 }
