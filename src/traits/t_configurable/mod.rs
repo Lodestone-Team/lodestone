77e6ff5504a91aeb969b3a11cdf3c66361580d0a
Peter <2892084432j@gmail.com>
2023-03-29 03:38:59 -0400

    remove unneeded methods

--- src/traits/t_configurable/mod.rs
+++ src/traits/t_configurable/mod.rs
@@ -76,7 +76,6 @@ pub trait TConfigurable {
     // getters
     async fn uuid(&self) -> InstanceUuid;
     async fn name(&self) -> String;
-    async fn flavour(&self) -> String;
     async fn game_type(&self) -> Game;
     async fn version(&self) -> String;
     async fn description(&self) -> String;
