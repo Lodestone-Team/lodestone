30ee50a308f70952777e58f441c12dcd125d03f5
Peter <2892084432j@gmail.com>
2023-05-08 01:25:21 -0400

    add file icons

--- src/components/FileViewer/FileList.tsx
+++ src/components/FileViewer/FileList.tsx
@@ -8,9 +8,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ClientFile } from 'bindings/ClientFile';
 import clsx from 'clsx';
 import Checkbox from 'components/Atoms/Checkbox';
-import { formatTimeAgo } from 'utils/util';
+import { formatTimeAgo, supportedZip } from 'utils/util';
 import FileContextMenu from './FileContextMenu';
-import React, { useState, useEffect, useRef, } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useEventListener, useOnClickOutside } from 'usehooks-ts';
 import { UnzipOption } from 'bindings/UnzipOptions';
 
@@ -48,8 +48,8 @@ export default function FileList({
   error: Error | null;
   tickedFiles: ClientFile[];
   tickFile: (file: ClientFile, ticked: boolean) => void;
-  zipFiles: (files: ClientFile[], dest : string) => void;
-  unzipFile: (file: ClientFile, unzipOption : UnzipOption) => void;
+  zipFiles: (files: ClientFile[], dest: string) => void;
+  unzipFile: (file: ClientFile, unzipOption: UnzipOption) => void;
   openedFile: ClientFile | null;
   atTopLevel: boolean;
   onParentClick: () => void;
@@ -74,32 +74,41 @@ export default function FileList({
     );
   };
 
-  const boundingDivRef = useRef<HTMLDivElement>(null) 
-  const contextMenuRef = useRef<HTMLDivElement>(null) 
-  const [mousePos, setMousePos] = useState<{x: number, y: number}>({x: 0, y: 0});
+  const boundingDivRef = useRef<HTMLDivElement>(null);
+  const contextMenuRef = useRef<HTMLDivElement>(null);
+  const [mousePos, setMousePos] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
   const [showContextMenu, setShowContextMenu] = useState(false);
-  const [contextMenuCoords, setContextMenuCoords] = useState({x: 0, y: 0});
+  const [contextMenuCoords, setContextMenuCoords] = useState({ x: 0, y: 0 });
   const [contextMenuFile, setContextMenuFile] = useState<ClientFile | null>();
-  const [absCoords, setAbsCoords] = useState({x: 0, y: 0})
-  const [boundingDivDimensions, setBoundingDivDimensions] = useState({ height: 0, width: 0})
+  const [absCoords, setAbsCoords] = useState({ x: 0, y: 0 });
+  const [boundingDivDimensions, setBoundingDivDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
 
-  const contextMenuDimensionsWithoutUnzip = { height: 259, width: 176 } // no unzip in menu (file name is null)
-  const contextMenuDimensionsWithUnzip = { height: 337, width: 176 } 
+  const contextMenuDimensionsWithoutUnzip = { height: 259, width: 176 }; // no unzip in menu (file name is null)
+  const contextMenuDimensionsWithUnzip = { height: 337, width: 176 };
 
   useEffect(() => {
     if (boundingDivRef.current !== null) {
-      setBoundingDivDimensions({ height: boundingDivRef.current.offsetHeight, width: boundingDivRef.current.offsetWidth })
+      setBoundingDivDimensions({
+        height: boundingDivRef.current.offsetHeight,
+        width: boundingDivRef.current.offsetWidth,
+      });
     }
   }, [boundingDivRef]);
 
   useEffect(() => {
     if (boundingDivRef.current !== null) {
       setAbsCoords({
-        x: boundingDivRef.current.getBoundingClientRect().left + window.scrollX, 
-        y: boundingDivRef.current.getBoundingClientRect().top + window.scrollY
+        x: boundingDivRef.current.getBoundingClientRect().left + window.scrollX,
+        y: boundingDivRef.current.getBoundingClientRect().top + window.scrollY,
       });
     }
-  }, [])
+  }, []);
 
   const onMouseMove = (e: MouseEvent) => {
     setMousePos({ x: e.clientX - absCoords.x, y: e.clientY - absCoords.y });
@@ -108,31 +117,102 @@ export default function FileList({
   const onResize = () => {
     if (boundingDivRef.current !== null) {
       setAbsCoords({
-          x: boundingDivRef.current.getBoundingClientRect().left + window.scrollX, 
-          y: boundingDivRef.current.getBoundingClientRect().top + window.scrollY 
+        x: boundingDivRef.current.getBoundingClientRect().left + window.scrollX,
+        y: boundingDivRef.current.getBoundingClientRect().top + window.scrollY,
       });
-      setBoundingDivDimensions({ height: boundingDivRef.current.offsetHeight, width: boundingDivRef.current.offsetWidth });
+      setBoundingDivDimensions({
+        height: boundingDivRef.current.offsetHeight,
+        width: boundingDivRef.current.offsetWidth,
+      });
     }
-  }
+  };
 
   useEventListener('mousemove', onMouseMove);
   useEventListener('resize', onResize);
   useEventListener('mousedown', onResize);
   useOnClickOutside(contextMenuRef, () => setShowContextMenu(false));
 
-  const calculateContextMenuCoords = (fileName? : string) => {
+  function fileToIcon(file: ClientFile) {
+    // a map from file extension to icon
+    const iconMap: { [key: string]: string } = {
+      txt: '/icons/document.svg',
+      md: '/icons/document.svg',
+      properties: '/icons/document.svg',
+      yml: '/icons/document.svg',
+      yaml: '/icons/document.svg',
+      cfg: '/icons/document.svg',
+      conf: '/icons/document.svg',
+      config: '/icons/document.svg',
+      ini: '/icons/document.svg',
+      png : '/icons/image.svg',
+      jpg : '/icons/image.svg',
+      jpeg : '/icons/image.svg',
+      gif : '/icons/image.svg',
+      bmp : '/icons/image.svg',
+      svg : '/icons/image.svg',
+      json: '/icons/json.svg',
+      zip: '/icons/zip.svg',
+      rar: '/icons/zip.svg',
+      gz: '/icons/zip.svg',
+      tar: '/icons/zip.svg',
+      jar: '/icons/jar.svg',
+      java: '/icons/java.svg',
+      ts: '/icons/typescript.svg',
+      js: '/icons/javascript.svg',
+      readme: '/icons/readme.svg',
+      bin: '/icons/3d.svg',
+      dat: '/icons/3d.svg',
+      dat_old: '/icons/3d.svg',
+      obj: '/icons/3d.svg',
+      nbt: '/icons/3d.svg',
+      mca: '/icons/3d.svg',
+      lock: '/icons/lock.svg',
+      log : '/icons/log.svg',
+    };
+
+    if (file.file_type === 'Directory') {
+      return (
+        <img src="/icons/folder-blue.svg" alt="folder icon" draggable="false" />
+      );
+    } else if (file.file_type === 'File') {
+      const fileExt = file.extension ?? '';
+      if (fileExt in iconMap) {
+        return (
+          <img
+            src={iconMap[fileExt]}
+            alt="file icon"
+            draggable="false"
+            className="h-5 w-5"
+          />
+        );
+      } else {
+        return (
+          <img
+            src="/icons/template.svg"
+            alt="file icon"
+            draggable="false"
+            className="h-5 w-5"
+          />
+        );
+      }
+    }
+
+    return (
+      <FontAwesomeIcon icon={faClipboardQuestion} className="text-yellow" />
+    );
+  }
+
+  const calculateContextMenuCoords = (file?: ClientFile) => {
     let x = null;
     let y = null;
     let width = 0;
     let height = 0;
-    const zipTypes = ["rar", "zip", "7z", "tar", "gz", "xz", "bz2", "tbz2", "tgz", "txz", "tlz", "lz"];
-    const fileType = fileName?.split('.').pop();
+    const fileExt = file?.extension ?? '';
 
-    if (zipTypes.includes(fileType ? fileType : '')) {
+    if (supportedZip.includes(fileExt)) {
       width = contextMenuDimensionsWithUnzip.width;
       height = contextMenuDimensionsWithUnzip.height;
-    }
-    else {
+    } else {
       width = contextMenuDimensionsWithoutUnzip.width;
       height = contextMenuDimensionsWithoutUnzip.height;
     }
@@ -140,35 +220,40 @@ export default function FileList({
     if (mousePos.x + width > boundingDivDimensions.width) {
       x = mousePos.x - width;
     } else {
-      x = mousePos.x
+      x = mousePos.x;
     }
     if (mousePos.y + height > boundingDivDimensions.height - 10) {
-      y = boundingDivDimensions.height - height - 10
+      y = boundingDivDimensions.height - height - 10;
     } else {
-      y = mousePos.y
+      y = mousePos.y;
     }
-    if (mousePos.x + width > boundingDivDimensions.width && mousePos.x - width < 4) {
+    if (
+      mousePos.x + width > boundingDivDimensions.width &&
+      mousePos.x - width < 4
+    ) {
       x = 4;
     }
-    
-    setContextMenuCoords({ x, y })
-    
-  }
+
+    setContextMenuCoords({ x, y });
+  };
 
   const fileTreeEntryClassName =
     'flex flex-row items-center gap-4 py-2 px-4 text-medium font-medium tracking-medium whitespace-nowrap';
 
   return (
-    <div className="flex h-full w-full grow flex-col @container/file-tree" ref={boundingDivRef}>
-      { showContextMenu && 
-        <FileContextMenu 
-          ref={contextMenuRef} 
+    <div
+      className="flex h-full w-full grow flex-col @container/file-tree"
+      ref={boundingDivRef}
+    >
+      {showContextMenu && (
+        <FileContextMenu
+          ref={contextMenuRef}
           currentPath={path}
-          file={contextMenuFile as ClientFile} 
-          coords={contextMenuCoords} 
-          setCreateFileModalOpen={setCreateFileModalOpen} 
-          setRenameFileModalOpen={setRenameFileModalOpen} 
-          setCreateFolderModalOpen={setCreateFolderModalOpen} 
+          file={contextMenuFile as ClientFile}
+          coords={contextMenuCoords}
+          setCreateFileModalOpen={setCreateFileModalOpen}
+          setRenameFileModalOpen={setRenameFileModalOpen}
+          setCreateFolderModalOpen={setCreateFolderModalOpen}
           setShowContextMenu={setShowContextMenu}
           setClipboard={setClipboard}
           zipFiles={zipFiles}
@@ -181,8 +266,8 @@ export default function FileList({
           pasteFiles={pasteFiles}
           deleteSingleFile={deleteSingleFile}
           deleteTickedFiles={deleteTickedFiles}
-        /> 
-      }
+        />
+      )}
       <div className="overflow-y-overlay flex h-0 grow flex-col divide-y divide-gray-faded/30 overflow-x-hidden">
         {!atTopLevel ? (
           <div
@@ -221,11 +306,12 @@ export default function FileList({
               'bg-gray-700': fileTicked(file) && file !== openedFile,
               'bg-gray-800': !fileTicked(file) && file !== openedFile,
             })}
-            onContextMenu={(e) => { e.preventDefault(); 
+            onContextMenu={(e) => {
+              e.preventDefault();
               setContextMenuFile(file);
-              calculateContextMenuCoords(file.name);
+              calculateContextMenuCoords(file);
               setShowContextMenu(true);
-              setModalPath(file.file_type === "Directory" ? file.path : path);
+              setModalPath(file.file_type === 'Directory' ? file.path : path);
             }}
           >
             <Checkbox
@@ -234,28 +320,14 @@ export default function FileList({
                 tickFile(file, ticked);
               }}
             />
-            <div className="w-3">
-              {file.file_type === 'Directory' && (
-                <FontAwesomeIcon icon={faFolder} className="text-blue-200" />
-              )}
-              {file.file_type === 'File' && (
-                <FontAwesomeIcon
-                  icon={openedFile?.path === file.path ? faFilePen : faFile}
-                  className="text-gray-400"
-                />
-              )}
-              {file.file_type === 'Unknown' && (
-                <FontAwesomeIcon
-                  icon={faClipboardQuestion}
-                  className="text-yellow"
-                />
-              )}
-            </div>
+            <div className="h-4 w-4 flex-shrink-0">{fileToIcon(file)}</div>
             <p
               className={clsx(
                 'truncate text-gray-300 hover:cursor-pointer hover:text-blue-200 hover:underline',
                 openedFile?.path === file.path && 'italic',
-                clipboardAction === 'cut' && clipboard?.some(f => f.path === file.path) && 'text-opacity-60 ',
+                clipboardAction === 'cut' &&
+                  clipboard?.some((f) => f.path === file.path) &&
+                  'text-opacity-60 '
               )}
               onClick={() => onFileClick(file)}
             >
@@ -272,10 +344,11 @@ export default function FileList({
             </p>
           </div>
         ))}
-        <div 
-          onClick={onEmptyClick} 
+        <div
+          onClick={onEmptyClick}
           className="min-h-[25%] grow"
-          onContextMenu={(e) => { e.preventDefault(); 
+          onContextMenu={(e) => {
+            e.preventDefault();
             setContextMenuFile(null);
             calculateContextMenuCoords();
             setShowContextMenu(true);
