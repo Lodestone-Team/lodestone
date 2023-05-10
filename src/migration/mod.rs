2852fae1a6ac14923893352e2ef319da30347de6
Peter <2892084432j@gmail.com>
2023-05-10 02:02:59 -0400

    new migration logic

--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -1,12 +1,20 @@
-mod v042_to_v043;
+mod v042_to_v044;
+pub mod v043_to_v044;
 
 use std::path::{Path, PathBuf};
 
 use color_eyre::eyre::Context;
 
-use tracing::debug;
+use serde::Deserialize;
+use tracing::{debug, info};
 
-use crate::{error::Error, implementations::minecraft::Flavour, types::InstanceUuid};
+use crate::{
+    error::Error,
+    implementations::minecraft::Flavour,
+    prelude::VERSION,
+    traits::t_configurable::GameType,
+    types::{InstanceUuid, LodestoneMetadata},
+};
 
 #[derive(serde::Deserialize, Clone)]
 pub struct RestoreConfigV042 {
@@ -29,117 +37,97 @@ pub struct RestoreConfigV042 {
     pub has_started: bool,
 }
 
-pub async fn migrate(path_to_instance: &Path) -> Result<(), Error> {
-    let path_to_dot_lodestone_config = path_to_instance.join(".lodestone_config");
-    let dot_lodestone_config: serde_json::Value = serde_json::from_reader(
-        std::fs::File::open(&path_to_dot_lodestone_config).context(format!(
-            "Failed to open config file at {}",
-            &path_to_dot_lodestone_config.display()
-        ))?,
-    )
-    .context(format!(
-        "Failed to deserialize config from {}. Was the config file modified manually?",
-        path_to_instance.display()
-    ))?;
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+pub struct DotLodestoneConfigV043 {
+    pub game_type: GameType,
+    pub uuid: InstanceUuid,
+    pub creation_time: i64,
+    pub lodestone_version: String,
+}
 
-    #[allow(clippy::single_match)]
-    match dot_lodestone_config.get("lodestone_version") {
-        None => {
-            // Version 0.4.2 did not have a version field in the config
-            debug!(
-                "Migrating {} from v0.4.2 to v0.4.3",
-                path_to_instance.display()
-            );
-            v042_to_v043::migrate_v042_to_v043(dot_lodestone_config, path_to_instance).await?;
+/// Versions without a lodestone_metadata.json file
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum LegacyVersion {
+    V042,
+    V043,
+}
+
+fn determine_legacy_version(lodestone_path: &Path) -> Result<Option<LegacyVersion>, Error> {
+    let metadata_path = lodestone_path.join(".lodestone_metadata.json");
+    // if the metadata exists, then it's not a legacy version
+    if metadata_path.is_file() {
+        Ok(None)
+    } else {
+        // check if there is at least one instance with a .lodestone_config file
+        let instances_path = lodestone_path.join("instances");
+        if !instances_path.is_dir() {
+            return Ok(None);
         }
-        _ => {}
+        let mut at_least_one_instance = false;
+        for entry in std::fs::read_dir(instances_path)
+            .context("Failed to read instances directory")?
+            .filter_map(|entry| entry.ok())
+        {
+            at_least_one_instance = true;
+            let path = entry.path();
+            if path.is_dir() && path.join(".lodestone_minecraft_config.json").is_file() {
+                return Ok(Some(LegacyVersion::V043));
+            }
+        }
+        if at_least_one_instance {
+            Ok(Some(LegacyVersion::V042))
+        } else {
+            Ok(None)
+        }
     }
+}
+
+/// Older version of Lodestone Core (v0.4.3 and below) does not store the version of Lodestone Core explicitly in version file.
+///
+/// More specifically, anything below v0.4.2 does not store version anywhere at all
+///
+/// While 0.4.3 only stores it via the `lodestone_version` field in instances' `.lodestone_config` file
+///
+/// From 0.4.4 onwards, the version stored under LODESTONE_PATH/version.json
+///
+/// The high-level migration process is as follows:
+///
+/// First check if the version file exists. If it does, then we can assume that the instance is at least v0.4.4
+///
+/// If the version file does not exist, then check if the `instances` directory has at least one instance with a `.lodestone_config` that contains the `lodestone_version` field
+///
+/// If it is, then we are at v0.4.3 and thus migrate to 0.4.4 by creating the version file
+/// and rewrite all the `.lodestone_config` files to remove the `lodestone_version` field
+///
+///
+
+pub fn migrate(lodestone_path: &Path) -> Result<(), Error> {
+    let legacy_version = determine_legacy_version(lodestone_path)?;
+    debug!("Legacy version: {:?}", legacy_version);
+    match legacy_version {
+        Some(LegacyVersion::V042) => {
+            info!("Migrating from v0.4.2 to v0.4.3");
+            v042_to_v044::migrate_v042_to_v044(&lodestone_path.join("instances"))?;
+        }
+        Some(LegacyVersion::V043) => {
+            info!("Migrating from v0.4.3 to v0.4.4");
+            v043_to_v044::migrate_v043_to_v044(&lodestone_path.join("instances"))?;
+        }
+        None => {
+            info!("No migration needed");
+            return Ok(());
+        }
+    }
+    let version_path = lodestone_path.join(".lodestone_metadata.json");
+    let version_file =
+        std::fs::File::create(version_path).context("Failed to create version file")?;
+    serde_json::to_writer_pretty(
+        version_file,
+        &LodestoneMetadata {
+            semver: VERSION.with(|v| v.clone()),
+        },
+    )
+    .context("Failed to write version file")?;
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-
-    use serde_json::json;
-
-    use crate::{
-        implementations::minecraft::{Flavour, RestoreConfig},
-        traits::t_configurable::GameType,
-        types::DotLodestoneConfig,
-    };
-
-    use super::migrate;
-
-    #[tokio::test]
-    async fn test_migrate_v042_to_v043() {
-        // setup temp dir
-        let temp_dir = tempdir::TempDir::new("test_migrate_v042_to_v043").unwrap();
-        let path_to_instance = temp_dir.path().join("test_instance");
-        std::fs::create_dir(&path_to_instance).unwrap();
-
-        // write old config
-        let old_dot_lodestone_config = json!({
-            "game_type": "minecraft",
-            "uuid": "a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6",
-            "name": "test_instance",
-            "version": "1.1.1",
-            "flavour": "fabric",
-            "description": "test instance",
-            "cmd_args": [],
-            "path": "/home/user/.minecraft",
-            "port": 25565,
-            "min_ram": 1024,
-            "max_ram": 1024,
-            "creation_time": 1,
-            "auto_start": false,
-            "restart_on_crash": false,
-            "backup_period": null,
-            "jre_major_version": 11,
-            "has_started": false
-        });
-
-        let path_to_dot_lodestone_config = path_to_instance.join(".lodestone_config");
-        let path_to_dot_lodestone_minecraft_config =
-            path_to_instance.join(".lodestone_minecraft_config.json");
-
-        std::fs::write(
-            &path_to_dot_lodestone_config,
-            old_dot_lodestone_config.to_string(),
-        )
-        .unwrap();
-
-        // migrate
-
-        migrate(path_to_instance.as_path()).await.unwrap();
-
-        // check new config
-
-        let new_dot_lodestone_config: DotLodestoneConfig =
-            serde_json::from_reader(std::fs::File::open(&path_to_dot_lodestone_config).unwrap())
-                .unwrap();
-
-        let new_dot_lodestone_minecraft_config: RestoreConfig = serde_json::from_reader(
-            std::fs::File::open(&path_to_dot_lodestone_minecraft_config).unwrap(),
-        )
-        .unwrap();
-
-        assert_eq!(
-            new_dot_lodestone_config.uuid().as_ref(),
-            "a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6"
-        );
-        assert_eq!(new_dot_lodestone_config.creation_time() as u64, 1);
-        assert_eq!(new_dot_lodestone_config.lodestone_version(), "0.4.3");
-        assert_eq!(
-            new_dot_lodestone_config.game_type(),
-            &GameType::MinecraftJava
-        );
-
-        assert_eq!(
-            new_dot_lodestone_minecraft_config.flavour,
-            Flavour::Fabric {
-                loader_version: None,
-                installer_version: None,
-            }
-        );
-    }
-}
