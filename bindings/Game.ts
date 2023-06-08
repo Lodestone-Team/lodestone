57fcb957f850d6b7746bfe1fe15caf9479dbad76
Peter <2892084432j@gmail.com>
2023-06-08 01:00:24 -0400

    bindings

--- bindings/Game.ts
+++ bindings/Game.ts
@@ -2,4 +2,4 @@
 import type { GameType } from "./GameType";
 import type { MinecraftVariant } from "./MinecraftVariant";
 
-export type Game = { type: "MinecraftJava", variant: MinecraftVariant, } | { type: "Generic", game_name: GameType, game_display_name: string, };
+export type Game = { type: "MinecraftJava", variant: MinecraftVariant, } | { type: "MinecraftBedrock" } | { type: "Generic", game_name: GameType, game_display_name: string, };
