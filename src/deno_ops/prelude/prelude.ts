f849d8ed879714e76853c12254bf8252218ea9e0
Peter <2892084432j@gmail.com>
2023-06-10 00:50:00 -0400

    add documentation

--- src/deno_ops/prelude/prelude.ts
+++ src/deno_ops/prelude/prelude.ts
@@ -10,7 +10,9 @@ export function macroPid(): MacroPID {
 export function instanceUUID(): string | null {
     return __instance_uuid;
 }
-
+// Notifies the caller that the macro wishes to be run in the background.
+// This is a no-op if the macro is already running in the background, or called multiple times.
+// This function DOES NOT exit the macro.
 export function detach() {
     emiDetach(macroPid(), instanceUUID());
 }
