83e738ccb9b58044ecdbb8f61b51cf5b0ba8b6e4
Peter <2892084432j@gmail.com>
2023-06-12 04:42:10 -0400

    minor

--- src/deno_ops/prelude/prelude.ts
+++ src/deno_ops/prelude/prelude.ts
@@ -1,20 +1,12 @@
-import { MacroPID } from "../../../deno_bindings/MacroPID.ts";
-import { emitDetach } from "../events/events.ts";
+import { TaskPID } from "../../../deno_bindings/TaskPID.ts";
 
 
-declare const __macro_pid: MacroPID;
+declare const __macro_pid: TaskPID;
 declare const __instance_uuid: string | null;
-export function taskPid(): MacroPID {
+export function taskPid(): TaskPID {
     return __macro_pid;
 }
 
 export function instanceUUID(): string | null {
     return __instance_uuid;
-}
-
-// Notifies the caller that the macro wishes to be run in the background.
-// This is a no-op if the macro is already running in the background, or called multiple times.
-// This function DOES NOT exit the macro.
-export function detach() {
-    emitDetach(taskPid(), instanceUUID());
 }
