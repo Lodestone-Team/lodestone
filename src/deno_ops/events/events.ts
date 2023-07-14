a2a3120b45b95d3690f395986df82937e3d38a91
Peter <2892084432j@gmail.com>
2023-07-14 11:02:25 -0400

    minor changes to events

--- src/deno_ops/events/events.ts
+++ src/deno_ops/events/events.ts
@@ -3,6 +3,8 @@ import { TaskPID } from "../../../deno_bindings/TaskPID.ts";
 import * as InstanceControl from "../instance_control/instance_control.ts"
 import { InstanceEvent } from "../../../deno_bindings/InstanceEvent.ts";
 import { InstanceState } from "../../../deno_bindings/InstanceState.ts";
+import { ProgressionStartValue } from "../../../deno_bindings/ProgressionStartValue.ts";
+import { ProgressionEndValue } from "../../../deno_bindings/ProgressionEndValue.ts";
 
 // re-exports 
 export type { ClientEvent, TaskPID, InstanceControl, InstanceEvent, InstanceState };
@@ -63,3 +65,14 @@ export function emitStateChange(state: InstanceState, instanceName: string, inst
     ops.emit_state_change(instanceUuid, instanceName, state);
 }
 
+export function emitProgressionEventStart(progressionName : string, total : number | null, inner : ProgressionStartValue | null) : string {
+    return ops.emit_progression_event_start(progressionName, total, inner);
+}
+
+export function emitProgressiontEventUpdate(eventId : string, progressMessage : string, progress : number) {
+    ops.emit_progression_event_update(eventId, progressMessage, progress);
+}
+
+export function emitProgressionEventEnd(eventId : string, success : boolean, message : string | null, inner : ProgressionEndValue | null) {
+    ops.emit_progression_event_end(eventId, success, message, inner);
+}
